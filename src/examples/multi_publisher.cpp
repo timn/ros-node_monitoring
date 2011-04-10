405feb062d95a60d9d29329a4127038edef23124
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-04-11 01:07:59 +0200

    example: multi publisher

---
+++ src/examples/multi_publisher.cpp
@@ -0,0 +1,79 @@
+
+
+/***************************************************************************
+ *  multi_publisher.cpp - Example: publish multiple "nodes"
+ *
+ *  Created: Fri Apr 08 12:16:30 2011
+ *  Copyright  2011  Tim Niemueller [www.niemueller.de]
+ *             2011  SRI International
+ *             2011  Carnegie Mellon University
+ *             2011  Intel Labs Pittsburgh
+ *             2011  Columbia University in the City of New York
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the 3-clase BSD License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE file in the root directory.
+ */
+
+#include <ros/ros.h>
+#include <nodemon/state_publisher.h>
+
+
+ros::Publisher g_pub;
+std::map<std::string, int> g_nodes;
+
+unsigned int msgnum = 0;
+
+void timer_cb(const ros::WallTimerEvent& event)
+{
+  std::map<std::string, int>::iterator p;
+  for (p = g_nodes.begin(); p != g_nodes.end(); ++p) {
+    nodemon_msgs::NodeState msg;
+    msg.nodename = p->first;
+    msg.state    = p->second;
+    msg.time = ros::Time(event.current_real.sec, event.current_real.nsec);
+    msg.message  = "";
+
+    if (msg.state == nodemon_msgs::NodeState::FATAL) {
+      msg.message  = "FATAL";
+    } else if (msg.state == nodemon_msgs::NodeState::ERROR) {
+      msg.message  = "ERROR";
+    } else if (msg.state == nodemon_msgs::NodeState::RECOVERING) {
+      msg.message  = "RECOVERING";
+    }
+
+    g_pub.publish(msg);
+  }
+}
+
+
+int
+main(int argc, char **argv)
+{
+  ros::init(argc, argv, "nodemon_pubex");
+  ros::NodeHandle n;
+
+  g_pub = n.advertise<nodemon_msgs::NodeState>("/nodemon/state", 10);
+
+  ros::WallTimer t = n.createWallTimer(ros::WallDuration(1.0), timer_cb);
+
+  g_nodes["/test1"] = nodemon_msgs::NodeState::STARTING;
+  g_nodes["/test2"] = nodemon_msgs::NodeState::RUNNING;
+  g_nodes["/test3"] = nodemon_msgs::NodeState::RECOVERING;
+  g_nodes["/test4"] = nodemon_msgs::NodeState::ERROR;
+  g_nodes["/test5"] = nodemon_msgs::NodeState::FATAL;
+  g_nodes["/test6"] = nodemon_msgs::NodeState::RUNNING;
+  g_nodes["/test7"] = nodemon_msgs::NodeState::RUNNING;
+  g_nodes["/test8"] = nodemon_msgs::NodeState::RUNNING;
+  g_nodes["/test9"] = nodemon_msgs::NodeState::STOPPING;
+
+  ros::spin();
+}
